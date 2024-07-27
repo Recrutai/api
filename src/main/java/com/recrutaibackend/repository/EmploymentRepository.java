619171476f128e64108818def95023c910a80d93
Mateus <90358717+mtsfer@users.noreply.github.com>
2024-07-27 20:12:22 -0300

    Corrige tipos dos IDs nas classes de repositorio (#3)

--- src/main/java/com/recrutaibackend/repository/EmploymentRepository.java
+++ src/main/java/com/recrutaibackend/repository/EmploymentRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmploymentRepository extends JpaRepository<Employment, Long> {
+public interface EmploymentRepository extends JpaRepository<Employment, Integer> {
 }
