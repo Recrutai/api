1019827640f8826af65e8dc348a2212cc6023087
hvenanc <hvs.git@gmail.com>
2024-07-28 19:13:50 -0300

    Commit para o merge

--- src/main/java/com/recrutaibackend/config/WebConfig.java
+++ src/main/java/com/recrutaibackend/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/*")
+                registry.addMapping("/**")
                         .allowedOrigins("*")
                         .allowedMethods("*")
                         .allowedHeaders("*")
