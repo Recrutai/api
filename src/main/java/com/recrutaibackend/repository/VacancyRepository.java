4d4bc9a92b1ed9f9e1bc37e31b66815d747cbb60
mtsfer <mtshs.ferraz@gmail.com>
2024-08-10 20:23:49 -0300

    Altera `findAllByTitle` para ignorar casing

--- src/main/java/com/recrutaibackend/repository/VacancyRepository.java
+++ src/main/java/com/recrutaibackend/repository/VacancyRepository.java
@@ -5,12 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface VacancyRepository extends JpaRepository<Vacancy, Integer> {
 
-    Optional<Vacancy> findByTitle(String title);
-
-    List<Vacancy> findAllByTitleContains(String title);
+    List<Vacancy> findAllByTitleContainsIgnoreCase(String title);
 }
