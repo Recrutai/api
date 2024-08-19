9ebd6c56bf8cdcb235306ebc98540b6a83d3a92e
mtsfer <mtshs.ferraz@gmail.com>
2024-08-19 20:43:34 -0300

    Adiciona migration para hidratar banco de dados

---
+++ src/main/resources/db/migration/V2_1__Hydrate_Database_SP2.sql
@@ -0,0 +1,22 @@
+INSERT INTO tb_employment_type(name)
+VALUES ('Contrato'),
+       ('Freelance'),
+       ('Estágio'),
+       ('Full-time'),
+       ('Part-time'),
+       ('Terceirizado');
+
+INSERT INTO tb_institution_size(lower_bound, upper_bound)
+VALUES (1, 1),
+       (2, 10),
+       (11, 50),
+       (51, 200),
+       (201, 1000),
+       (1001, 5000),
+       (5001, 10000),
+       (10000);
+
+INSERT INTO tb_industry(name)
+VALUES ('Tecnologia'),
+       ('Pesquisa e Inovação'),
+       ('Administração');
