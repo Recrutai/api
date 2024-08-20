6eb0bf8ebe772fe1dc4d3d3688e0351f9db43312
Mateus <90358717+mtsfer@users.noreply.github.com>
2024-08-19 21:18:39 -0300

    Implementa busca de membros e refatora cadastro (#53)

--- src/main/java/com/recrutaibackend/repository/MemberRepository.java
+++ src/main/java/com/recrutaibackend/repository/MemberRepository.java
@@ -4,6 +4,11 @@ import com.recrutaibackend.model.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    List<Member> findAllByInstitutionId(Long institutionId);
+
 }
--- src/main/java/com/recrutaibackend/service/MemberService.java
+++ src/main/java/com/recrutaibackend/service/MemberService.java
@@ -1,7 +1,9 @@
 package com.recrutaibackend.service;
 
 import com.recrutaibackend.dto.MemberRequest;
+import com.recrutaibackend.dto.MemberResponse;
 import com.recrutaibackend.mappers.MemberMapper;
+import com.recrutaibackend.model.Institution;
 import com.recrutaibackend.model.Member;
 import com.recrutaibackend.repository.MemberRepository;
 import jakarta.transaction.Transactional;
@@ -9,35 +11,41 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
+
 @Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
     private final UserService userService;
-    private final InstitutionService institutionService;
     private final MemberMapper memberMapper;
 
     public MemberService(
             MemberRepository memberRepository,
             UserService userService,
-            InstitutionService institutionService,
             MemberMapper memberMapper
     ) {
         this.memberRepository = memberRepository;
         this.userService = userService;
-        this.institutionService = institutionService;
         this.memberMapper = memberMapper;
     }
 
     @Transactional
-    public Member create(MemberRequest request) {
+    public MemberResponse create(Institution institution, MemberRequest request) {
         var user = userService.findById(request.userId());
-        var institution = institutionService.findById(request.institutionId());
         var addedBy = this.findById(request.addedById());
 
         var member = memberMapper.mapToEntity(request, user, institution, addedBy);
+        var savedMember = memberRepository.save(member);
 
-        return memberRepository.save(member);
+        return memberMapper.mapToResponse(savedMember);
+    }
+
+    public List<MemberResponse> findAllByInstitutionId(long institutionId) {
+        return memberRepository.findAllByInstitutionId(institutionId)
+                .stream()
+                .map(memberMapper::mapToResponse)
+                .toList();
     }
 
     public Member findById(long id) {
