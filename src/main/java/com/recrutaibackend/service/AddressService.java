a8f2c386d99c24e77bc7fa0668558ad3fe18e912
mtsfer <mtshs.ferraz@gmail.com>
2024-08-22 15:51:27 -0300

    Remove endpoints de endereco
    Todo endereco na aplicacao esta associado a algum recurso, nao existindo
    por conta propria. Criar endpoints de endereco, sem estar associado a
    algum recurso, vai contra a organizacao da API.

--- src/main/java/com/recrutaibackend/service/AddressService.java
+++ src/main/java/com/recrutaibackend/service/AddressService.java
@@ -1,15 +1,10 @@
 package com.recrutaibackend.service;
 
 import com.recrutaibackend.dto.AddressRequest;
-import com.recrutaibackend.dto.AddressResponse;
 import com.recrutaibackend.mappers.AddressMapper;
 import com.recrutaibackend.model.Address;
 import com.recrutaibackend.repository.AddressRepository;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.List;
 
 @Service
 public class AddressService {
@@ -25,16 +20,6 @@ public class AddressService {
     public Address create(AddressRequest addressRequest) {
         var address = addressMapper.mapToEntity(addressRequest);
         return addressRepository.save(address);
-    }
-
-    public List<Address> findAll() {
-        return addressRepository.findAll();
-    }
-
-    public AddressResponse findByStreetAddress(String streetAddress) {
-        var address = addressRepository.findByStreetAddress(streetAddress)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Address not found!"));
-        return addressMapper.mapToResponse(address);
     }
 
 }
