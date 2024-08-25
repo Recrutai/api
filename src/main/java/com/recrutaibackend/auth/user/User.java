97082943ccb1286368cc1f5641f15fa509f1b146
hvenanc <hvs.git@gmail.com>
2024-08-25 10:41:03 -0300

    Correção do Login - Mudança do FetchType.Lazy para FetchType.EAGER

--- src/main/java/com/recrutaibackend/auth/user/User.java
+++ src/main/java/com/recrutaibackend/auth/user/User.java
@@ -35,7 +35,7 @@ public class User {
     @Column(name = "hashed_password")
     private String hashedPassword;
 
-    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumn(name = "location_id")
     private Address location;
 
