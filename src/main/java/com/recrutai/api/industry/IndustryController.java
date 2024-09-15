ed2c70ac24693872c89f9ee78e060fc295067717
Mateus <90358717+mtsfer@users.noreply.github.com>
2024-09-15 10:12:30 -0300

    Implementa busca de industrias por nome (#70)

--- src/main/java/com/recrutai/api/industry/IndustryController.java
+++ src/main/java/com/recrutai/api/industry/IndustryController.java
@@ -1,14 +1,18 @@
 package com.recrutai.api.industry;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/industries")
@@ -20,11 +24,15 @@ public class IndustryController {
         this.industryService = industryService;
     }
 
-    @Operation(summary = "Get available industries")
-    @ApiResponse(responseCode = "200", description = "OK", useReturnTypeSchema = true)
+    @Operation(summary = "Search through available industries")
+    @ApiResponse(
+            responseCode = "200",
+            description = "OK",
+            content = @Content(examples = @ExampleObject("[\"Technology\", \"Education\", \"Administration\"]"))
+    )
     @GetMapping
-    ResponseEntity<List<String>> findAll() {
-        var industries = industryService.findAll();
+    ResponseEntity<List<String>> findAllFiltered(@RequestParam Optional<String> name) {
+        var industries = industryService.findAllFiltered(name);
         return ResponseEntity.ok(industries);
     }
 
