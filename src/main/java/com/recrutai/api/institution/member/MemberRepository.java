eb7e1d6c62a5b3582cd47f6f0d81494c1a9e2f6c
mtsfer <mtshs.ferraz@gmail.com>
2024-09-17 09:36:17 -0300

    Altera para retornar apenas membros ativos

--- src/main/java/com/recrutai/api/institution/member/MemberRepository.java
+++ src/main/java/com/recrutai/api/institution/member/MemberRepository.java
@@ -2,6 +2,7 @@ package com.recrutai.api.institution.member;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,7 +10,16 @@ import java.util.List;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    List<Member> findAllByInstitutionId(Long institutionId);
+    @Query("""
+            SELECT m FROM Member m
+            JOIN FETCH m.user u
+            LEFT JOIN FETCH u.location ul
+            LEFT JOIN FETCH m.addedBy ab
+            LEFT JOIN FETCH ab.user abu
+            LEFT JOIN FETCH abu.location abul
+            WHERE m.institution.id = :institutionId AND m.removedAt IS NULL
+            """)
+    List<Member> findAllByInstitutionId(@Param("institutionId") long institutionId);
 
     @Query(value = """
             SELECT EXISTS(
@@ -19,6 +29,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
                 AND removed_at IS NULL
             )
             """, nativeQuery = true)
-    boolean exists(long institutionId, long userId);
+    boolean exists(@Param("institutionId") long institutionId, @Param("userId") long userId);
 
 }
